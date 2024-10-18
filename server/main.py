668aa8a529bb073b091cfa1304149af235440a35
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-10-18 17:11:43 -0500

    inital commit

---
+++ server/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {
+        "message": "Welcome to the Team 18 Abide",
+    }
+
+
+# Debugging Middleware
+@app.middleware("http")
+async def cors_debug(request, call_next):
+    response = await call_next(request)
+    print(f"Request: {request.method} {request.url} - CORS Headers: {response.headers}")
+    return response
