66ada359a083d3f4c184040ea668177de58ab71a
Rubenbaral <Rubenbaral3@gmail.com>
2024-10-19 00:35:32 -0500

    push admin needs fix

---
+++ client/src/app/admin/medical-dash/page.js
@@ -0,0 +1,12 @@
+import Medical-DB from '@/components/Medical-DB'
+import React from 'react'
+
+function Page() {
+    return (
+        <div>
+            <Medical-DB/>
+        </div>
+    )
+}
+
+export default Page
