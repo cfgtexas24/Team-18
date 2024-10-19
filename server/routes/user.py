700d89002068b9ea421fac9114ea1f1db3ad871d
Nisha Jakkinpali <npj3@duke.edu>
2024-10-18 19:57:32 -0500

    change to appt

--- server/routes/user.py
+++ server/routes/user.py
@@ -10,7 +10,8 @@ router = APIRouter()
 async def user_appts():
     mock_appointments = [
         {"id": 1, "user_id": 1, "date": datetime.datetime(2023, 10, 1, 10, 0), "description": "Dentist appointment"},
-       
+        {"id": 2, "user_id": 1, "date": datetime.datetime(2023, 10, 2, 12, 0), "description": "Therapy session"},
+        {"id": 3, "user_id": 2, "date": datetime.datetime(2023, 10, 3, 14, 0), "description": "Doctor appointment"},
     ]
     return mock_appointments
 
