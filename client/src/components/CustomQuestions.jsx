5a70137d319421f223d6db0301893e4c75cdd277
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-10-19 08:27:08 -0500

    remove lab reports

--- client/src/components/CustomQuestions.jsx
+++ client/src/components/CustomQuestions.jsx
@@ -44,7 +44,7 @@ export default function CustomQuestions() {
 
   const handleAddQuestion = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:8000/questions/', {
+      const response = await fetch('http://127.0.0.1:8000/questions', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
