22b5807f413e31064a30c354e6fbb1c9133eeebe
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-10-18 19:41:28 -0500

    more description

--- server/routes/user.py
+++ server/routes/user.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, FastAPI
+import datetime
 # Create the FastAPI app
 app = FastAPI()
 # Initialize the APIRouter
@@ -7,12 +8,17 @@ router = APIRouter()
 # Define user-related routes
 @router.get("/user/appt")
 async def user_appts():
-    return None
+    mock_appointments = [
+        {"id": 1, "user_id": 1, "date": datetime.datetime(2023, 10, 1, 10, 0), "description": "Dentist appointment"},
+       
+    ]
+    return mock_appointments
 
-@router.get("/user/reports")
-async def user_reports():
-    return None
+# @router.get("/user/reports")
+# async def user_reports():
+#     return None
 
+# body is datetime, user_id, location
 @router.post("/user/schedule")
 async def user_schedule():
     return None
